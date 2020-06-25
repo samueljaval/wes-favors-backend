4cd32149f3de817180bf00f428d603dcd7f31dbc
Samuel Javal <samueljaval@gmail.com>
2020-06-25 23:35:10 +0200

    login/signin done

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,10 @@ require("dotenv").config()
 
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
+let SECRET = process.env.MONGO_URI + "thisisthesecretkey" //should probably have it in .env
 
 module.exports = {
 	MONGODB_URI,
-	PORT
+	PORT,
+    SECRET
 }
