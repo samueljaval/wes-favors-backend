a2ea17014e3adcbd8059fbde7c6385c09734612d
Samuel Javal <samueljaval@gmail.com>
2020-06-30 13:13:31 +0200

    not working yetg

--- tests/favors_api.test.js
+++ tests/favors_api.test.js
@@ -7,95 +7,148 @@ const Favor = require('../models/favor')
 const User = require('../models/user')
 const jwt = require("jsonwebtoken")
 
-
-
-
-
 beforeEach(async () => {
-  await Favor.deleteMany({})
-  await User.deleteMany({})
-
-
-  const favorObjects = helper.initialFavors
-    .map(favor => new Favor(favor))
-  const promiseArray = favorObjects.map(favor => favor.save())
-  await Promise.all(promiseArray)
+    await Favor.deleteMany({})
+    await User.deleteMany({})
+    const favorObjects = helper.initialFavors.map(favor => new Favor(favor))
+    const promiseArray = favorObjects.map(favor => favor.save())
+    await Promise.all(promiseArray)
 })
 
-test('a lot of stuff', async () => {
-    // create alpiazza 13 and sam_javal users, make sure it works
-    await api
-    .post('/api/users')
-    .send({username: "alpiazza13", password: "password"})
-    .expect(201)
+describe('sign up users', () => {
 
-    await api
-      .post('/api/users')
-      .send({username: "sam_javal", password: "password2"})
-      .expect(201)
+    test('proper user sign up and no duplicates', async () => {
 
-      // make sure you can't add new user with same username
-      await api
-        .post('/api/users')
-        .send({username: "sam_javal", password: "password3"})
-        .expect(500)
+        await api
+            .post('/api/users')
+            .send({email: "alpggiaz3@wesleyan.edu",
+                   password: "passworD23",
+                   name : 'AP',
+                   class : 2020,
+                   phone : 8609876789,
+               })
+            .expect(201)
+        const users = await helper.usersInDb()
+        console.log(users)
+        await api
+            .post('/api/users')
+            .send({email: "alpioioggiaz3@wesleyan.edu",
+                   password: "passworD23",
+                   name : 'AP',
+                   class : 2020,
+                   phone : 8609876789,
+               })
+            .expect(201)
 
-    // make sure 2 users were added to db and they have the right usernames
-    const users = await helper.usersInDb()
-    expect(users[0].username).toBe("alpiazza13")
-    expect(users[1].username).toBe("sam_javal")
-    expect(users.length).toEqual(2)
+        const users2 = await helper.usersInDb()
+        console.log(users2)
 
-    // log in alpiazza13
-    await api
-    .post('/api/login')
-    .send({username: "alpiazza13", password: "password"})
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+        //
+        // await api
+        //     .post('/api/users')
+        //     .send({email: "alpiazza13@wesleyan.edu",
+        //            password: "passworD23",
+        //            name : 'AP',
+        //            class : 2020,
+        //            phone : 8609876789,
+        //         })
+        //     .expect(500)
+        //
+        // const users = await helper.usersInDb()
+        // expect(users[0].email).toBe("alpiazza13@wesleyan.edu")
+        // expect(users[1].email).toBe("sam_javal@wesleyan.edu")
+        // expect(users.length).toEqual(2)
+    })
 
-    // make sure wrong password fails
-    await api
-    .post('/api/login')
-    .send({username: "alpiazza13", password: "password12"})
-    .expect(401)
+    test('wrong email at signup', async () => {
+        await api
+            .post('/api/users')
+            .send({email: "sam_javal",
+                   password: "password24E",
+                   name : 'AP',
+                   class : 2020,
+                   phone : 8609876789,
+                })
+            .expect(400)
+    })
 
-    // get alex's info and token
-    const alex = {username: users[0].username, id: users[0].id}
-    const token = jwt.sign(alex, process.env.SECRET)
+    test('wrong password at signup', async () => {
+        await api
+            .post('/api/users')
+            .send({email: "sam_javal@wesleyan.edu",
+                   password: "password",
+                   name : 'AP',
+                   class : 2020,
+                   phone : 8609876789,
+                })
+            .expect(400)
+    })
 
-    // make sure get request of favors works with alex's credentials
-    await api
-    .get('/api/favors')
-    .set('Authorization', 'bearer ' + token)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    test('not enough info at signup', async () => {
+        await api
+            .post('/api/users')
+            .send({email: "alpia3@wesleyan.edu",
+                   password: "password35T",
+                   name : 'AP',
+                })
+            .expect(500)
+    })
 
-    // create favor using alex's account
-    await api
-    .post('/api/favors')
-    .send( {title: 'fake title 1', details: 'fake details 1'} )
-    .set('Authorization', 'bearer ' + token)
-    .expect(201)
-
-    // make sure favore was added to db
-    const favorsNow = await helper.favorsInDb()
-    expect(favorsNow.length).toEqual(2)
-
-    // make sure favor was added to database with the right title
-    const onlyTitles = favorsNow.map(favor => favor.title)
-    expect(onlyTitles).toContain('fake title 1')
-
-    // make sure alex's id was added to that favor as the requester
-    expect(favorsNow[1].requester.toString()).toBe(alex.id)
-
-    // make sure alex's favors list now has that favor's id
-    const id = favorsNow[1].id
-    const usersNow = await helper.usersInDb()
-    const alexFavors = usersNow[0].favors_requested
-    expect(alexFavors[0].toString()).toBe(id)
-
-    // still need to test deleting, commenting accepting
 })
+//
+// test('a lot of stuff', async () => {
+//
+//
+//     // log in alpiazza13
+//     await api
+//     .post('/api/login')
+//     .send({username: "alpiazza13", password: "password"})
+//     .expect(200)
+//     .expect('Content-Type', /application\/json/)
+//
+//     // make sure wrong password fails
+//     await api
+//     .post('/api/login')
+//     .send({username: "alpiazza13", password: "password12"})
+//     .expect(401)
+//
+//     // get alex's info and token
+//     const alex = {username: users[0].username, id: users[0].id}
+//     const token = jwt.sign(alex, process.env.SECRET)
+//
+//     // make sure get request of favors works with alex's credentials
+//     await api
+//     .get('/api/favors')
+//     .set('Authorization', 'bearer ' + token)
+//     .expect(200)
+//     .expect('Content-Type', /application\/json/)
+//
+//     // create favor using alex's account
+//     await api
+//     .post('/api/favors')
+//     .send( {title: 'fake title 1', details: 'fake details 1'} )
+//     .set('Authorization', 'bearer ' + token)
+//     .expect(201)
+//
+//     // make sure favore was added to db
+//     const favorsNow = await helper.favorsInDb()
+//     expect(favorsNow.length).toEqual(2)
+//
+//     // make sure favor was added to database with the right title
+//     const onlyTitles = favorsNow.map(favor => favor.title)
+//     expect(onlyTitles).toContain('fake title 1')
+//
+//     // make sure alex's id was added to that favor as the requester
+//     expect(favorsNow[1].requester.toString()).toBe(alex.id)
+//
+//     // make sure alex's favors list now has that favor's id
+//     const id = favorsNow[1].id
+//     const usersNow = await helper.usersInDb()
+//     const alexFavors = usersNow[0].favors_requested
+//     expect(alexFavors[0].toString()).toBe(id)
+//
+//     // still need to test deleting, commenting accepting
+// })
 
 
 afterAll(() => {
