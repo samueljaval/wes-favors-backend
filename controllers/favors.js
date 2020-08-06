ab0959a0ee904fc41042e8a1bcfea5e26475add5
Samuel Javal <samueljaval@gmail.com>
2020-08-06 10:11:43 +0200

    categories

--- controllers/favors.js
+++ controllers/favors.js
@@ -130,12 +130,12 @@ favorsRouter.put('/accept/:id', async (req, res, next) => {
             const requester = await User.findById(favor.requester)
             console.log(requester.phone)
             //send text message
-            // await twilioClient.messages
-            //   .create({
-            //      body: 'Hi ' + requester.name + '! ' + user.name + ' has accepted your favor titled: ' + favor.title + '! Here is their phone number: ' + user.phone.toString(),
-            //      from: process.env.TWILIO_PHONE_NUMBER,
-            //      to: requester.phone
-            //    })
+            await twilioClient.messages
+              .create({
+                 body: 'Hi ' + requester.name + '! ' + user.name + ' has accepted your favor titled: ' + favor.title + '! Here is their phone number: ' + user.phone.toString(),
+                 from: process.env.TWILIO_PHONE_NUMBER,
+                 to: requester.phone
+               })
             await favor.save()
             user.favors_accepted.push(req.params.id)
             await user.save()
