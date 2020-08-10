314112b5509d73bbad67fa420b284482eb509ac7
Samuel Javal <samueljaval@gmail.com>
2020-08-10 15:37:08 +0200

    google

--- controllers/google.js
+++ controllers/google.js
@@ -4,29 +4,43 @@ const {OAuth2Client} = require('google-auth-library')
 const jwt = require("jsonwebtoken")
 
 
-const client = new OAuth2Client()
+const client = new OAuth2Client(process.env.CLIENT_ID)
 
 googleRouter.post("/", async (req, res) => {
     const tokenId = req.body.tokenId
     const response = await client.verifyIdToken({idToken : tokenId, audience : process.env.CLIENT_ID})
+    let email_verified = null
+    let email = null
     if (response) {
-        console.log("eee")
-        let email_verified = null
-        let email = null
-        if (response) {
-            email_verified = response.payload.email_verified
-            email = response.payload.email
-        }
-        else {
-            return res.status(400).send({error : "problem with Google OAuth"})
-        }
-        if (email_verified) {
-            // || true is for testing
-            // || true is for testing
-            // || true is for testing
-            if (email.includes("@wesleyan.edu") || true) {
+        email_verified = response.payload.email_verified
+        email = response.payload.email
+    }
+    else {
+        return res.status(400).send({error : "problem with Google OAuth"})
+    }
+    if (email_verified) {
+        // || true is for testing
+        // || true is for testing
+        // || true is for testing
+        if (email.includes("@wesleyan.edu") || true) {
+            const user = await User.findOne({email})
+            if (user) {
+                 console.log('user')
+                const userForToken = {
+    				email: email,
+    				id: user._id,
+    			}
+    			const token = jwt.sign(userForToken, process.env.SECRET)
+                res.status(200).send({ token })
+            }
+            else {
+                const newUser = new User({
+                    email: email,
+                    new_user: true
+                })
+                const response = await newUser.save()
                 const user = await User.findOne({email})
-                if (user) {
+                if (response) {
                     const userForToken = {
         				email: email,
         				id: user._id,
@@ -35,32 +49,13 @@ googleRouter.post("/", async (req, res) => {
                     res.status(200).send({ token })
                 }
                 else {
-                    const newUser = new User({
-                        email: email,
-                        new_user: true
-                    })
-                    const response = await newUser.save()
-                    const user = await User.findOne({email})
-                    if (response) {
-                        const userForToken = {
-            				email: email,
-            				id: user._id,
-            			}
-            			const token = jwt.sign(userForToken, process.env.SECRET)
-                        res.status(200).send({ token })
-                    }
-                    else {
-                        res.status(400).send({ error : "problem creating new user" })
-                    }
+                    res.status(400).send({ error : "problem creating new user" })
                 }
             }
-            else {
-                return res.status(400).send({ error : "You need a wesleyan.edu google account  " })
-            }
         }
-    }
-    else {
-        return res.status(400).rend({error : "Google OAuth did not work"})
+        else {
+            return res.status(400).send({ error : "You need a wesleyan.edu google account  " })
+        }
     }
 })
 
