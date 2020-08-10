d88ffb64435271b9bfe8de99621f4315dcd32fd3
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-08-10 00:29:31 -0400

    deleting user

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,13 @@
 const usersRouter = require("express").Router()
 const User = require("../models/user")
+const Favor = require("../models/favor")
+const Comment = require("../models/comments")
 const bcrypt = require("bcrypt")
 const crypto = require('crypto')
 const Token = require("../models/emailToken")
-const mailgun = require("mailgun-js");
+const mailgun = require("mailgun-js")
+const jwt = require('jsonwebtoken')
+
 
 // checking if password is at least 8 char long,
 // has at leastno one upperCase, one lowerCase and a digit
@@ -12,6 +16,25 @@ const passCheck = (str) => {
 			&& (/[A-Z]/.test(str))
 			&& (/[0-9]/.test(str))
 			&& str.length >= 8
+}
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+const getUser = async req => {
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return res.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    return user
 }
 
 usersRouter.post("/", async (req, res, next) => {
@@ -80,6 +103,36 @@ usersRouter.post("/", async (req, res, next) => {
             }
 		}
 		else {return res.status(400).send({ error : "password needs to be defined" })}
+	}
+	catch (error) {
+		next(error)
+	}
+
+})
+
+usersRouter.delete("/:id", async (req, res, next) => {
+	try {
+        const user = await getUser(req)
+        const idToDelete = req.params.id
+
+        if (user.id != idToDelete) {
+            return res.status(400).send({error: "to delete a user you must be logged in as that user"})
+        }
+
+        const found_user = await User.findById(idToDelete)
+        if (!found_user) {
+            return res.status(400).send({error: "no user exists with the id specified"})
+        }
+
+        await User.findByIdAndRemove(idToDelete)
+        await Favor.deleteMany({ requester: idToDelete})
+        await Comment.deleteMany({ user: idToDelete})
+        const allFavorsBad = await Favor.find({})
+        const allFavors = allFavorsBad.map(favor => favor.toJSON())
+        const favorsToUpdate = allFavors.filter(favor => favor.completer == idToDelete)
+        const onlyIds = favorsToUpdate.map(favor => favor.id)
+        onlyIds.map(async (id) => {await Favor.findByIdAndUpdate(id, {accepted: false, completer: null})})
+        return res.status(201).send({success: "user successfully deleted"})
 	}
 	catch (error) {
 		next(error)
